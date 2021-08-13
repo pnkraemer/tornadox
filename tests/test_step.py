c3aa5e17c1f0c0f3140ef15cb948b028969354b3
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2021-08-13 14:34:21 +0200

    adaptive steps test

--- tests/test_step.py
+++ tests/test_step.py
@@ -19,4 +19,73 @@ def test_constant_steps():
     assert proposed == 0.1
     assert steprule.is_accepted(scaled_error_estimate=0.1)
 
+def test_adaptive_steps():
+    abstol = 0.1
+    reltol = 0.01
+    steprule = tornado.step.AdaptiveSteps(first_dt=0.1, abstol=abstol, reltol=reltol)
+    assert isinstance(steprule, tornado.step.AdaptiveSteps)
 
+    # < 1 accepted, > 1 rejected
+    assert steprule.is_accepted(scaled_error_estimate=0.99)
+    assert not steprule.is_accepted(scaled_error_estimate=1.01)
+
+    # Accepting a step makes the next one larger
+    assert steprule.suggest(previous_dt=0.3, scaled_error_estimate=0.5, local_convergence_rate=2) > 0.3
+    assert steprule.suggest(previous_dt=0.3, scaled_error_estimate=2.0, local_convergence_rate=2) < 0.3
+
+    # Error estimation to scaled norm 1d
+    unscaled_error_estimate = jnp.array([0.5])
+    reference_state = jnp.array([2.0])
+    E = steprule.errorest_to_norm(unscaled_error_estimate=unscaled_error_estimate, reference_state=reference_state)
+    scaled_error = unscaled_error_estimate / (abstol + reltol * reference_state)
+    assert jnp.allclose(E, scaled_error)
+
+    # Error estimation to scaled norm 2d
+    unscaled_error_estimate = jnp.array([0.5, 0.6])
+    reference_state = jnp.array([2.0, 3.0])
+    E = steprule.errorest_to_norm(unscaled_error_estimate=unscaled_error_estimate, reference_state=reference_state)
+    scaled_error = jnp.linalg.norm(unscaled_error_estimate / (abstol + reltol * reference_state)) / jnp.sqrt(2)
+    assert jnp.allclose(E, scaled_error)
+
+#
+# # since Adaptive Steps are different now...
+# class TestAdaptiveStep(unittest.TestCase):
+#     """We pretend that we have a solver of local error rate three and see if steps are
+#     proposed accordingly."""
+#
+#     def test_errorest_to_norm_1d(self):
+#         errorest = 0.5
+#         reference_state = np.array(2.0)
+#         expected = errorest / (self.atol + self.rtol * reference_state)
+#         received = self.asr.errorest_to_norm(errorest, reference_state)
+#         self.assertAlmostEqual(expected, received)
+#
+#     def test_errorest_to_norm_2d(self):
+#         errorest = np.array([0.1, 0.2])
+#         reference_state = np.array([2.0, 3.0])
+#         expected = np.linalg.norm(
+#             errorest / (self.atol + self.rtol * reference_state)
+#         ) / np.sqrt(2)
+#         received = self.asr.errorest_to_norm(errorest, reference_state)
+#         self.assertAlmostEqual(expected, received)
+#
+#     def test_minstep_maxstep(self):
+#         adaptive_steps = diffeq.stepsize.AdaptiveSteps(
+#             firststep=1.0,
+#             limitchange=(0.0, 1e10),
+#             minstep=0.1,
+#             maxstep=10,
+#             atol=1,
+#             rtol=1,
+#         )
+#
+#         with self.assertRaises(RuntimeError):
+#             adaptive_steps.suggest(
+#                 laststep=1.0, scaled_error=100_000.0, localconvrate=1
+#             )
+#         with self.assertRaises(RuntimeError):
+#             adaptive_steps.suggest(
+#                 laststep=1.0, scaled_error=1.0 / 100_000.0, localconvrate=1
+#             )
+#
+#
--- tornado/step.py
+++ tornado/step.py
@@ -26,7 +26,7 @@ class StepRule(abc.ABC):
         raise NotImplementedError
 
     def errorest_to_norm(
-        self, scaled_error_estimate, reference_state
+            self, unscaled_error_estimate, reference_state
     ):
         raise NotImplementedError
 
@@ -47,72 +47,60 @@ class ConstantSteps(StepRule):
         return True
     #
     # def errorest_to_norm(
-    #     self, errorest: ToleranceDiffusionType, reference_state: np.ndarray
+    #         self, unscaled_error_estimate, reference_state
     # ):
     #     pass
 
 
 
+class AdaptiveSteps(StepRule):
 
+    def __init__(
+        self,
+        first_dt, abstol, reltol, max_changes = (0.2, 10.0), safety_scale= 0.95, min_step = 1e-15, max_step = 1e15
+    ):
+        super().__init__(first_dt=first_dt)
+        self.abstol = abstol
+        self.reltol = reltol
+        self.max_changes = max_changes
+        self.safety_scale = safety_scale
+        self.min_step = min_step
+        self.max_step = max_step
 
-#
-# # since Adaptive Steps are different now...
-# class TestAdaptiveStep(unittest.TestCase):
-#     """We pretend that we have a solver of local error rate three and see if steps are
-#     proposed accordingly."""
-#
-#     def setUp(self):
-#         """Set up imaginative solver of convergence rate 3."""
-#         self.atol = 0.1
-#         self.rtol = 0.01
-#         self.asr = diffeq.stepsize.AdaptiveSteps(
-#             firststep=1.0, atol=self.atol, rtol=self.rtol
-#         )
-#
-#     def test_is_accepted(self):
-#         errorest = 0.5  # < 1, should be accepted
-#         self.assertTrue(self.asr.is_accepted(errorest))
-#
-#     def test_suggest(self):
-#         """If errorest <1, the next step should be larger."""
-#         step = 0.55 * random_state.rand()
-#         errorest = 0.75
-#         sugg = self.asr.suggest(step, errorest, localconvrate=3)
-#         self.assertGreater(sugg, step)
-#
-#     def test_errorest_to_norm_1d(self):
-#         errorest = 0.5
-#         reference_state = np.array(2.0)
-#         expected = errorest / (self.atol + self.rtol * reference_state)
-#         received = self.asr.errorest_to_norm(errorest, reference_state)
-#         self.assertAlmostEqual(expected, received)
-#
-#     def test_errorest_to_norm_2d(self):
-#         errorest = np.array([0.1, 0.2])
-#         reference_state = np.array([2.0, 3.0])
-#         expected = np.linalg.norm(
-#             errorest / (self.atol + self.rtol * reference_state)
-#         ) / np.sqrt(2)
-#         received = self.asr.errorest_to_norm(errorest, reference_state)
-#         self.assertAlmostEqual(expected, received)
-#
-#     def test_minstep_maxstep(self):
-#         adaptive_steps = diffeq.stepsize.AdaptiveSteps(
-#             firststep=1.0,
-#             limitchange=(0.0, 1e10),
-#             minstep=0.1,
-#             maxstep=10,
-#             atol=1,
-#             rtol=1,
-#         )
-#
-#         with self.assertRaises(RuntimeError):
-#             adaptive_steps.suggest(
-#                 laststep=1.0, scaled_error=100_000.0, localconvrate=1
-#             )
-#         with self.assertRaises(RuntimeError):
-#             adaptive_steps.suggest(
-#                 laststep=1.0, scaled_error=1.0 / 100_000.0, localconvrate=1
-#             )
-#
-#
+    def suggest(
+            self, previous_dt, scaled_error_estimate, local_convergence_rate=None
+    ):
+        if local_convergence_rate is None:
+            raise ValueError("Please provide a local convergence rate.")
+
+        small, large = self.max_changes
+
+        ratio = 1.0 / scaled_error_estimate
+        change = self.safety_scale * ratio ** (1.0 / local_convergence_rate)
+
+        # The below code should be doable in a single line?
+        if change < small:
+            dt = small * previous_dt
+        elif large < change:
+            dt = large * previous_dt
+        else:
+            dt = change * previous_dt
+
+        if dt < self.min_step:
+            raise ValueError("Step-size smaller than minimum step-size")
+        if dt > self.max_step:
+            raise ValueError("Step-size larger than maximum step-size")
+        return dt
+
+    def is_accepted(self, scaled_error_estimate):
+        return scaled_error_estimate < 1
+
+    def errorest_to_norm(
+            self, unscaled_error_estimate, reference_state
+    ):
+        if unscaled_error_estimate.shape != reference_state.shape:
+            raise ValueError("Unscaled error estimate needs same shape as reference state.")
+        tolerance = self.abstol + self.reltol * reference_state
+        ratio = unscaled_error_estimate / tolerance
+        dim = len(ratio) if ratio.ndim > 0 else 1
+        return jnp.linalg.norm(ratio) / jnp.sqrt(dim)
