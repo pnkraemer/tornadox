25db839a995a5061aa63586822af181ed9817b88
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2021-08-13 13:30:25 +0200

    doc

--- tests/test_rv.py
+++ tests/test_rv.py
@@ -7,11 +7,10 @@ import jax.numpy
 
 
 def test_rv():
-
+    """Random variables work as expected."""
     mean = jax.numpy.array([1., 2.])
     cov_cholesky = jax.numpy.array([[1., 0.], [1., 1.]])
     normal = tornado.rv.MultivariateNormal(mean=mean, cov_cholesky=cov_cholesky)
 
     assert isinstance(normal, tornado.rv.MultivariateNormal)
-
     assert jax.numpy.allclose(normal.cov, cov_cholesky @ cov_cholesky.T)
