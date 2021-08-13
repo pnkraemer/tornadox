055498a74e683d879aea4b8abb296150f02c0b85
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2021-08-13 13:43:09 +0200

    vanderpol added

---
+++ tests/test_ivp.py
@@ -0,0 +1,24 @@
+"""Tests for initial value problems and examples thereof."""
+
+import pytest
+
+import tornado
+import jax.numpy
+
+IVPs = [tornado.ivp.vanderpol()]
+
+@pytest.mark.parametrize("ivp", IVPs)
+def test_ivp(ivp):
+    assert isinstance(ivp, tornado.ivp.InitialValueProblem)
+
+@pytest.mark.parametrize("ivp", IVPs)
+def test_f(ivp):
+    f = ivp.f(ivp.t0, ivp.y0)
+    assert isinstance(f, jax.numpy.ndarray)
+    assert f.shape == ivp.y0.shape
+
+@pytest.mark.parametrize("ivp", IVPs)
+def test_df(ivp):
+    df = ivp.df(ivp.t0, ivp.y0)
+    assert isinstance(df, jax.numpy.ndarray)
+
---
+++ tornado/ivp.py
@@ -0,0 +1,48 @@
+"""Initial value problems and examples."""
+
+
+from typing import Callable, Union, Optional
+import jax.numpy as jnp
+import jax
+import dataclasses
+
+
+
+@dataclasses.dataclass
+class InitialValueProblem:
+    """Initial value problems."""
+
+    f: Callable[[float, jnp.ndarray], jnp.ndarray]
+    t0: float
+    tmax: float
+    y0: Union[float, jnp.ndarray]
+    df: Callable[[float, jnp.ndarray], jnp.ndarray]
+
+
+    @property
+    def dimension(self):
+        if jnp.isscalar(self.y0):
+            return 1
+        return self.y0.shape[0]
+
+
+
+def vanderpol(t0=0.0, tmax=30, y0=None, stiffness_constant=1e1):
+
+
+    if y0 is None:
+        y0 = jnp.array([2.0, 0.0])
+
+    def vanderpol_rhs(Y):
+        return jnp.array([Y[1], stiffness_constant * (1.0 - Y[0] ** 2) * Y[1] - Y[0]])
+
+    df = jax.jacfwd(vanderpol_rhs)
+
+    def rhs(t, y):
+        return vanderpol_rhs(Y=y)
+
+    def jac(t, y):
+        return df(y)
+
+
+    return InitialValueProblem(f=rhs, t0=t0, tmax=tmax, y0=y0, df=jac)
