32dc8e371e2164111f0477f7dde0684f1ab296e8
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2021-08-13 16:33:44 +0200

    sqrt prediction

---
+++ tests/test_sqrt.py
@@ -0,0 +1,35 @@
+"""Tests for square-root utilities."""
+
+import pytest
+import tornado
+import jax.numpy as jnp
+
+import jax
+
+
+@pytest.fixture
+def iwp():
+    return tornado.iwp.IntegratedWienerTransition(
+        wiener_process_dimension=1, num_derivatives=2
+    )
+
+def test_predict(iwp):
+    transition_matrix, process_noise_cholesky = iwp.preconditioned_discretize_1d
+
+    # dummy cholesky factor
+    some_chol = process_noise_cholesky.copy()
+    chol = tornado.sqrt.predict(A=transition_matrix, SC=some_chol, SQ=process_noise_cholesky)
+    cov = transition_matrix @ some_chol @ some_chol.T @ transition_matrix.T + process_noise_cholesky @ process_noise_cholesky.T
+
+    assert jnp.allclose(chol @ chol.T, cov)
+    assert jnp.allclose(jnp.linalg.cholesky(cov), chol)
+
+def test_tril_to_positive_tril():
+
+    matrix = jnp.array([
+        [1., 0., 0.],
+        [-1., 0., 0.],
+        [1., 2., 3.],
+    ])
+    result = tornado.sqrt.tril_to_positive_tril(matrix)
+    assert jnp.allclose(matrix, result)
---
+++ tornado/sqrt.py
@@ -0,0 +1,29 @@
+"""Square-root transition utility functions."""
+
+import jax.numpy as jnp
+import jax
+
+def predict(A, SC, SQ):
+    """Compute Cholesky factor of A @ SC @ SC.T @ A.T + SQ @ SQ.T"""
+    S1 = A @ SC
+
+    stacked_up = jnp.vstack((S1.T, SQ.T))
+    upper_sqrtm = jnp.linalg.qr(stacked_up, mode="r")
+    lower_sqrtm = upper_sqrtm.T
+    return tril_to_positive_tril(lower_sqrtm)
+
+
+def tril_to_positive_tril(tril_mat):
+    r"""Orthogonally transform a lower-triangular matrix into a lower-triangular matrix with positive diagonal.
+    In other words, make it a valid lower Cholesky factor.
+    The name of the function is based on `np.tril`.
+    """
+    diag = jnp.diag(tril_mat)
+    d = jnp.sign(diag)
+
+    # Like numpy, JAX assigns sign 0 to 0.0, which eliminate entire rows in the operation below.
+    d = jax.ops.index_add(d, d==0, 1.0)
+
+    # Fast(er) multiplication with a diagonal matrix from the right via broadcasting.
+    with_pos_diag = tril_mat * d[None, :]
+    return with_pos_diag
