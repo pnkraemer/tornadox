92441464fbe89e6309b6838aa163d9ea026b98bc
Nicholas Krämer <nicokraemer@gmx.net>
2021-08-15 08:39:10 +0200

    full solve adaptive ek1

--- tests/test_ek1.py
+++ tests/test_ek1.py
@@ -109,3 +109,23 @@ def test_diagonal_ek1_adaptive_steps():
     assert step_diag.reference_state.shape == (d,)
     assert step_diag.error_estimate.shape == (d,)
     assert jnp.all(step_diag.reference_state > 0)
+
+
+def test_diagonal_ek1_adaptive_steps_full_solve():
+
+    ivp = tornado.ivp.vanderpol(t0=0.0, tmax=0.25, stiffness_constant=1.0)
+    scipy_sol = solve_ivp(ivp.f, t_span=(ivp.t0, ivp.tmax), y0=ivp.y0)
+    final_t_scipy = scipy_sol.t[-1]
+    final_y_scipy = scipy_sol.y[:, -1]
+
+    dt = jnp.mean(jnp.diff(scipy_sol.t))
+    steps = tornado.step.AdaptiveSteps(first_dt=dt, abstol=1e-3, reltol=1e-3)
+    ek1 = tornado.ek1.DiagonalEK1(num_derivatives=4, ode_dimension=2, steprule=steps)
+    sol_gen = ek1.solution_generator(ivp=ivp)
+    for state in sol_gen:
+        pass
+
+    final_t_ek1 = state.t
+    final_y_ek1 = ek1.P0 @ state.y.mean
+    assert jnp.allclose(final_t_scipy, final_t_ek1)
+    assert jnp.allclose(final_y_scipy, final_y_ek1, rtol=1e-3, atol=1e-3)
