d6066bd0c5abca7140046d1c1f2789654b24c9cf
Nicholas Krämer <nicokraemer@gmx.net>
2021-08-15 09:26:37 +0200

    sqrt update simplified

--- tests/test_sqrt.py
+++ tests/test_sqrt.py
@@ -27,6 +27,11 @@ def SC(iwp):
     return iwp.preconditioned_discretize_1d[1]
 
 
+@pytest.fixture
+def batch_size():
+    return 3
+
+
 @pytest.mark.parametrize("measurement_style", ["full", "partial"])
 def test_propagate_cholesky_factor(H_and_SQ, SC, measurement_style):
     H, SQ = H_and_SQ
@@ -39,94 +44,69 @@ def test_propagate_cholesky_factor(H_and_SQ, SC, measurement_style):
     assert jnp.all(jnp.diag(chol) > 0)
 
 
-def test_batched_propagate_cholesky_factors(iwp):
-    transition_matrix, process_noise_cholesky = iwp.preconditioned_discretize_1d
-    A = tornado.linops.BlockDiagonal(jnp.stack([transition_matrix] * 3))
+@pytest.mark.parametrize("measurement_style", ["full", "partial"])
+def test_batched_propagate_cholesky_factors(
+    H_and_SQ, SC, measurement_style, batch_size
+):
 
-    # dummy cholesky factor
-    some_chol1 = tornado.linops.BlockDiagonal(
-        jnp.stack([process_noise_cholesky.copy()] * 3)
-    )
-    some_chol2 = tornado.linops.BlockDiagonal(
-        jnp.stack([process_noise_cholesky.copy()] * 3)
-    )
+    H, SQ = H_and_SQ
+    H = tornado.linops.BlockDiagonal(jnp.stack([H] * batch_size))
+    SQ = tornado.linops.BlockDiagonal(jnp.stack([SQ] * batch_size))
+    SC = tornado.linops.BlockDiagonal(jnp.stack([SC] * batch_size))
 
-    # First test: Non-optional S2
     chol = tornado.sqrt.batched_propagate_cholesky_factor(
-        (A @ some_chol1).array_stack, some_chol2.array_stack
+        (H @ SC).array_stack, SQ.array_stack
     )
     chol_as_bd = tornado.linops.BlockDiagonal(chol)
-    reference = tornado.sqrt.propagate_cholesky_factor(
-        A.todense() @ some_chol1.todense(), some_chol2.todense()
-    )
+    reference = tornado.sqrt.propagate_cholesky_factor((H @ SC).todense(), SQ.todense())
     assert jnp.allclose(chol_as_bd.todense(), reference)
 
 
-def test_batched_sqrtm_to_cholesky(iwp):
-    transition_matrix, process_noise_cholesky = iwp.preconditioned_discretize_1d
-    A = tornado.linops.BlockDiagonal(jnp.stack([transition_matrix] * 3))
-    d = transition_matrix.shape[0]
+@pytest.mark.parametrize("measurement_style", ["full", "partial"])
+def test_batched_sqrtm_to_cholesky(H_and_SQ, SC, measurement_style, batch_size):
+    H, SQ = H_and_SQ
+    d = H.shape[0]
+    H = tornado.linops.BlockDiagonal(jnp.stack([H] * batch_size))
+    SC = tornado.linops.BlockDiagonal(jnp.stack([SC] * batch_size))
 
-    # dummy cholesky factor
-    some_chol = tornado.linops.BlockDiagonal(
-        jnp.stack([process_noise_cholesky.copy()] * 3)
-    )
-    some_sqrtm = A @ some_chol
-
-    chol = tornado.sqrt.batched_sqrtm_to_cholesky(some_sqrtm.array_stack)
+    chol = tornado.sqrt.batched_sqrtm_to_cholesky((H @ SC).T.array_stack)
     chol_as_bd = tornado.linops.BlockDiagonal(chol)
 
-    reference = tornado.sqrt.sqrtm_to_cholesky(some_sqrtm.todense())
+    reference = tornado.sqrt.sqrtm_to_cholesky((H @ SC).T.todense())
     assert jnp.allclose(chol_as_bd.todense(), reference)
     assert chol_as_bd.array_stack.shape == (3, d, d)
 
 
-def test_tril_to_positive_tril():
-    """Assert that the weird sign(0)=0 behaviour is made up for."""
-    matrix = jnp.array(
-        [
-            [1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            [1.0, 2.0, 3.0],
-        ]
-    )
-    result = tornado.sqrt.tril_to_positive_tril(matrix)
-    assert jnp.allclose(matrix, result)
-
-
-def test_update_sqrt(iwp):
+@pytest.mark.parametrize("measurement_style", ["full", "partial"])
+def test_update_sqrt(H_and_SQ, SC, measurement_style):
     """Test the square-root updates."""
-    # Use sqrt(Q) as a dummy for a sqrt(C)
-    A, SC = iwp.preconditioned_discretize_1d
-    d = SC.shape[0]
 
-    # Check square and non-square!
-    for H in [A, A[:1]]:
-        SC_new, kalman_gain, innov_chol = tornado.sqrt.update_sqrt(H, SC)
-        assert isinstance(SC_new, jnp.ndarray)
-        assert isinstance(kalman_gain, jnp.ndarray)
-        assert isinstance(innov_chol, jnp.ndarray)
-        assert SC_new.shape == (d, d)
-        assert kalman_gain.shape == (d, H.shape[0])
-        assert innov_chol.shape == (H.shape[0], H.shape[0])
+    H, _ = H_and_SQ
+    SC_new, kalman_gain, innov_chol = tornado.sqrt.update_sqrt(H, SC)
+    assert isinstance(SC_new, jnp.ndarray)
+    assert isinstance(kalman_gain, jnp.ndarray)
+    assert isinstance(innov_chol, jnp.ndarray)
+    assert SC_new.shape == SC.shape
+    assert kalman_gain.shape == (H.shape[1], H.shape[0])
+    assert innov_chol.shape == (H.shape[0], H.shape[0])
 
-        # expected:
-        S = H @ SC @ SC.T @ H.T
-        K = SC @ SC.T @ H.T @ jnp.linalg.inv(S)
-        C = SC @ SC.T - K @ S @ K.T
+    # expected:
+    S = H @ SC @ SC.T @ H.T
+    K = SC @ SC.T @ H.T @ jnp.linalg.inv(S)
+    C = SC @ SC.T - K @ S @ K.T
 
-        # Test SC
-        assert jnp.allclose(SC_new @ SC_new.T, C)
-        assert jnp.allclose(SC_new, jnp.tril(SC_new))
-        assert jnp.all(jnp.diag(SC_new) >= 0)
+    # Test SC
+    assert jnp.allclose(SC_new @ SC_new.T, C)
+    assert jnp.allclose(SC_new, jnp.tril(SC_new))
+    assert jnp.all(jnp.diag(SC_new) >= 0)
 
-        # Test K
-        assert jnp.allclose(K, kalman_gain)
+    # Test K
+    assert jnp.allclose(K, kalman_gain)
 
-        # Test S
-        assert jnp.allclose(innov_chol @ innov_chol.T, S)
-        assert jnp.allclose(innov_chol, jnp.tril(innov_chol))
-        assert jnp.all(jnp.diag(innov_chol) >= 0)
+    # Test S
+    assert jnp.allclose(innov_chol @ innov_chol.T, S)
+    assert jnp.allclose(innov_chol, jnp.tril(innov_chol))
+    assert jnp.all(jnp.diag(innov_chol) >= 0)
 
 
 def test_batched_update_sqrt(iwp):
@@ -170,3 +150,16 @@ def test_batched_update_sqrt(iwp):
             (chol_as_bd @ chol_as_bd.T).todense(), ref_chol @ ref_chol.T
         )
         assert jnp.all(jnp.diag(chol_as_bd.todense()) >= 0.0)
+
+
+def test_tril_to_positive_tril():
+    """Assert that the weird sign(0)=0 behaviour is made up for."""
+    matrix = jnp.array(
+        [
+            [1.0, 0.0, 0.0],
+            [-1.0, 0.0, 0.0],
+            [1.0, 2.0, 3.0],
+        ]
+    )
+    result = tornado.sqrt.tril_to_positive_tril(matrix)
+    assert jnp.allclose(matrix, result)
