79672c9b9193f3c85bf069e8938f55eabe139cd3
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2021-08-16 11:17:51 +0200

    refactored blockdiag tests

--- tests/test_linops.py
+++ tests/test_linops.py
@@ -3,27 +3,45 @@
 
 import jax.numpy as jnp
 import jax.scipy.linalg
+import pytest
 
 import tornado
 
 
-def test_block_diagonal():
+@pytest.fixture
+def A():
+    return jnp.arange(0, 9).reshape((3, 3))
 
-    A = jnp.arange(0, 9).reshape((3, 3))
-    B = jnp.arange(10, 19).reshape((3, 3))
+
+@pytest.fixture
+def B():
+    return jnp.arange(10, 19).reshape((3, 3))
+
+
+@pytest.fixture
+def sparse_dense_blockdiag(A, B):
     dense = jax.scipy.linalg.block_diag(A, B)
 
     # todense() works correctly
     sparse = tornado.linops.BlockDiagonal.from_arrays(A, B)
+    return sparse, dense
+
+
+def test_todense(sparse_dense_blockdiag):
+    sparse, dense = sparse_dense_blockdiag
     assert jnp.allclose(sparse.todense(), dense)
 
-    # matmul() works correctly with other linops
+
+def test_matmul_blockdiag_blockdiag(sparse_dense_blockdiag, A, B):
+    sparse, dense = sparse_dense_blockdiag
     expected = jax.scipy.linalg.block_diag(A @ A, B @ B)
     new = sparse @ sparse
     assert isinstance(new, tornado.linops.BlockDiagonal)
     assert jnp.allclose(new.todense(), expected)
 
-    # matvec() works correctly with jax.numpy.array
+
+def test_matvec_blodiag_array(sparse_dense_blockdiag):
+    sparse, dense = sparse_dense_blockdiag
     arr = jnp.arange(0, dense.shape[0])
     new = sparse @ arr
     expected = dense @ arr
@@ -31,7 +49,8 @@ def test_block_diagonal():
     assert new.shape == arr.shape
     assert jnp.allclose(new, expected)
 
-    # Sum of two block diagonals works
+
+def test_sum_block_diagonals(A, B):
     B1 = tornado.linops.BlockDiagonal.from_arrays(A, B)
     B2 = tornado.linops.BlockDiagonal.from_arrays(B, A)
     new = B1 + B2
