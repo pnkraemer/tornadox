c3d91be35158cdb0b3164a48bfee9c60ea3df6cb
Nicholas Krämer <nicokraemer@gmx.net>
2021-08-21 08:14:25 +0200

    refactored normal tests

--- tests/test_rv.py
+++ tests/test_rv.py
@@ -1,16 +1,42 @@
 """Tests for random variables."""
 
 
-import jax.numpy
+import jax.numpy as jnp
+import pytest
 
 import tornado
 
 
-def test_rv():
-    """Random variables work as expected."""
-    mean = jax.numpy.array([1.0, 2.0])
-    cov_cholesky = jax.numpy.array([[1.0, 0.0], [1.0, 1.0]])
-    normal = tornado.rv.MultivariateNormal(mean=mean, cov_sqrtm=cov_cholesky)
+@pytest.fixture
+def dimension():
+    return 3
 
-    assert isinstance(normal, tornado.rv.MultivariateNormal)
-    assert jax.numpy.allclose(normal.cov, cov_cholesky @ cov_cholesky.T)
+
+@pytest.fixture
+def batch_size():
+    return 5
+
+
+@pytest.fixture
+def mean(dimension):
+    return jnp.arange(dimension)
+
+
+@pytest.fixture
+def cov_sqrtm(dimension):
+    return jnp.arange(dimension ** 2).reshape((dimension, dimension))
+
+
+@pytest.fixture
+def multivariate_normal(mean, cov_sqrtm):
+    return tornado.rv.MultivariateNormal(mean=mean, cov_sqrtm=cov_sqrtm)
+
+
+def test_multivariate_normal_type(multivariate_normal):
+    assert isinstance(multivariate_normal, tornado.rv.MultivariateNormal)
+
+
+def test_multivariate_normal_cov(multivariate_normal):
+    SC = multivariate_normal.cov_sqrtm
+    C = multivariate_normal.cov
+    assert jnp.allclose(C, SC @ SC.T)
