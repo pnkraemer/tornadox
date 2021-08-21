0e7577e1ae55784b61fb3dcae3f80d26676ae5d0
Nicholas Krämer <nicokraemer@gmx.net>
2021-08-21 13:23:20 +0200

    ek0 solve test

--- tests/test_ek0.py
+++ tests/test_ek0.py
@@ -18,16 +18,33 @@ def steps():
 
 
 @pytest.fixture
+def num_derivatives():
+    return 4
+
+
+@pytest.fixture
+def ek0_solution(ek0_version, num_derivatives, ivp, steps):
+    ek0 = ek0_version(num_derivatives=num_derivatives, ode_dimension=2, steprule=steps)
+    sol_gen = ek0.solution_generator(ivp=ivp)
+    for state in sol_gen:
+        if state.t > ivp.t0:
+            pass
+
+    final_t_ek0 = state.t
+    # if isinstance(ek0, tornado.ek0.ReferenceEK0):
+    #     final_y_ek0 = ek0.P0 @ state.y.mean
+    # else:
+    #     final_y_ek0 = state.y.mean[0]
+    final_y_ek0 = ek0.P0 @ state.y.mean
+    return final_t_ek0, final_y_ek0
+
+
+@pytest.fixture
 def scipy_solution(ivp):
     scipy_sol = solve_ivp(ivp.f, t_span=(ivp.t0, ivp.tmax), y0=ivp.y0)
     final_t_scipy = scipy_sol.t[-1]
     final_y_scipy = scipy_sol.y[:, -1]
     return final_t_scipy, final_y_scipy
-
-
-@pytest.fixture
-def num_derivatives():
-    return 4
 
 
 # Tests for full solves.
@@ -42,24 +59,11 @@ all_ek0_versions = pytest.mark.parametrize("ek0_version", EK0_VERSIONS)
 
 
 @all_ek0_versions
-def test_full_solve_compare_scipy(
-    ek0_version, ivp, steps, scipy_solution, num_derivatives
-):
+def test_full_solve_compare_scipy(ek0_solution, scipy_solution):
     """Assert the ODEFilter solves an ODE appropriately."""
     final_t_scipy, final_y_scipy = scipy_solution
+    final_t_ek0, final_y_ek0 = ek0_solution
 
-    ek0 = ek0_version(num_derivatives=num_derivatives, ode_dimension=2, steprule=steps)
-    sol_gen = ek0.solution_generator(ivp=ivp)
-    for state in sol_gen:
-        if state.t > ivp.t0:
-            pass
-
-    final_t_ek0 = state.t
-    # if isinstance(ek0, tornado.ek0.ReferenceEK0):
-    #     final_y_ek0 = ek0.P0 @ state.y.mean
-    # else:
-    #     final_y_ek0 = state.y.mean[0]
-    final_y_ek0 = ek0.P0 @ state.y.mean
     assert jnp.allclose(final_t_scipy, final_t_ek0)
     assert jnp.allclose(final_y_scipy, final_y_ek0, rtol=1e-3, atol=1e-3)
 
