68eedae767311f2981d7e2fb780ffd9bd382b07e
Nicholas Krämer <nicokraemer@gmx.net>
2021-08-21 08:22:41 +0200

    batched mv normal

--- tests/test_rv.py
+++ tests/test_rv.py
@@ -40,3 +40,35 @@ def test_multivariate_normal_cov(multivariate_normal):
     SC = multivariate_normal.cov_sqrtm
     C = multivariate_normal.cov
     assert jnp.allclose(C, SC @ SC.T)
+
+
+@pytest.fixture
+def batched_mean(mean, batch_size):
+    return jnp.stack([mean] * batch_size)
+
+
+@pytest.fixture
+def batched_cov_sqrtm(cov_sqrtm, batch_size):
+    return jnp.stack([cov_sqrtm] * batch_size)
+
+
+@pytest.fixture
+def batched_multivariate_normal(batched_mean, batched_cov_sqrtm):
+    return tornado.rv.BatchedMultivariateNormal(
+        mean=batched_mean, cov_sqrtm=batched_cov_sqrtm
+    )
+
+
+def test_batched_multivariate_normal_type(batched_multivariate_normal):
+    assert isinstance(batched_multivariate_normal, tornado.rv.BatchedMultivariateNormal)
+
+
+def test_batched_multivariate_shapes(batched_multivariate_normal):
+    pass
+
+
+def test_batched_multivariate_normal_cov(batched_multivariate_normal):
+    batched_SC = batched_multivariate_normal.cov_sqrtm
+    batched_SC_T = jnp.transpose(batched_SC, axes=(0, 2, 1))
+    batched_C = batched_multivariate_normal.cov
+    assert jnp.allclose(batched_C, batched_SC @ batched_SC_T)
