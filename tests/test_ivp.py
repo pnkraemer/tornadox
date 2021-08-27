c53734c28d7c01f1d2a88e6665b1896e182dcaad
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2021-08-27 16:36:50 +0200

    test lorenz versions equivalent

--- tests/test_ivp.py
+++ tests/test_ivp.py
@@ -1,6 +1,6 @@
 """Tests for initial value problems and examples thereof."""
 
-import jax.numpy
+import jax.numpy as jnp
 import pytest
 
 import tornadox
@@ -21,7 +21,7 @@ def test_ivp(ivp):
 @pytest.mark.parametrize("ivp", IVPs)
 def test_f(ivp):
     f = ivp.f(ivp.t0, ivp.y0)
-    assert isinstance(f, jax.numpy.ndarray)
+    assert isinstance(f, jnp.ndarray)
     assert f.shape == ivp.y0.shape
 
 
@@ -29,5 +29,42 @@ def test_f(ivp):
 def test_df(ivp):
     if ivp.df is not None:
         df = ivp.df(ivp.t0, ivp.y0)
-        assert isinstance(df, jax.numpy.ndarray)
+        assert isinstance(df, jnp.ndarray)
         assert df.shape == (ivp.y0.shape[0], ivp.y0.shape[0])
+
+
+@pytest.fixture
+def both_lorenz_formulations():
+    lorenz = tornadox.ivp.lorenz96()
+    lorenz_loop = tornadox.ivp.lorenz96_loop()
+    return lorenz, lorenz_loop
+
+
+def test_lorenz_formulations_same_t0(both_lorenz_formulations):
+    lorenz, lorenz_loop = both_lorenz_formulations
+    assert lorenz.t0 == lorenz_loop.t0
+
+
+def test_lorenz_formulations_same_tmax(both_lorenz_formulations):
+    lorenz, lorenz_loop = both_lorenz_formulations
+    assert lorenz.tmax == lorenz_loop.tmax
+
+
+def test_lorenz_formulations_same_y0(both_lorenz_formulations):
+    lorenz, lorenz_loop = both_lorenz_formulations
+
+    assert jnp.allclose(lorenz.y0, lorenz_loop.y0)
+
+
+def test_lorenz_formulations_same_f(both_lorenz_formulations):
+    lorenz, lorenz_loop = both_lorenz_formulations
+    fx = lorenz.f(lorenz.t0, lorenz.y0)
+    fx_loop = lorenz_loop.f(lorenz_loop.t0, lorenz_loop.y0)
+    assert jnp.allclose(fx, fx_loop)
+
+
+def test_lorenz_formulations_same_df(both_lorenz_formulations):
+    lorenz, lorenz_loop = both_lorenz_formulations
+    dfx = lorenz.df(lorenz.t0, lorenz.y0)
+    dfx_loop = lorenz_loop.df(lorenz_loop.t0, lorenz_loop.y0)
+    assert jnp.allclose(dfx, dfx_loop)
