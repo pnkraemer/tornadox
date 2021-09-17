43669440bdcd74109fde08016d1a4c16e4f929f4
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2021-09-17 16:09:10 +0200

    Remvoe prints

--- tests/test_ivp.py
+++ tests/test_ivp.py
@@ -121,6 +121,4 @@ def test_jacobian():
     ivp = tornadox.ivp.fhn_2d()
     diag_df = jnp.diagonal(ivp.df(ivp.t0, ivp.y0))
     df_diag = ivp.df_diagonal(ivp.t0, ivp.y0)
-    print(diag_df)
-    print(df_diag)
     assert jnp.allclose(diag_df, df_diag)
