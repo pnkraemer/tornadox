862def3afdfde37cef5b1cfc14d6c408be387c49
Nicholas Krämer <nicokraemer@gmx.net>
2021-09-19 15:15:10 +0200

    added pde script to timingsÄ

--- timings/large_pde.py
+++ timings/large_pde.py
@@ -1,9 +1,14 @@
-"""This end-to-end test verifies that the ODE filters can be used for large PDEs."""
+"""Does it make sense to jit for large problems?
+
+
+This script evaluates (and benchmarks) whether the jitted perform step makes sense for LARGE problems.
+We can use it to check the efficiency of the implementation scheme in the future.
+"""
 
 import tornadox
 
 # 0.04 -> 20k dimensions
-fhn = tornadox.ivp.fhn_2d(bbox=[[-1.0, -1.0], [1.0, 1.0]], dx=0.02)
+fhn = tornadox.ivp.fhn_2d(bbox=[[-1.0, -1.0], [1.0, 1.0]], dx=0.04)
 steprule = tornadox.step.AdaptiveSteps(abstol=1e-3, reltol=1e-2)
 solver = tornadox.ek1.DiagonalEK1(
     num_derivatives=4,
@@ -13,12 +18,17 @@ solver = tornadox.ek1.DiagonalEK1(
 
 dt = tornadox.step.propose_first_dt(f=fhn.f, t0=fhn.t0, y0=fhn.y0)
 init = solver.initialize(fhn)
-solver.post_initialize(fhn)
 step_attempted, info_attempted = solver.attempt_step(init, dt, *fhn)
 step_performed, _, info_performed = solver.perform_full_step(init, dt, *fhn)
+step_performed_compiled, _, info_performed_compiled = solver.perform_full_step_compiled(
+    init, dt, *fhn
+)
 
 # %timeit solver.initialize(fhn)
-# %timeit solver.attempt_step(init, dt, *fhn)
-# %timeit solver.perform_full_step(init, dt, *fhn)
+# %timeit solver.attempt_step(init, dt, *fhn)  # 46 ms
+# %timeit solver.perform_full_step(init, dt, *fhn)  # 769 ms
+# %timeit solver.perform_full_step_compiled(init, dt, *fhn)  # 772 ms
 
-print(info_attempted, info_performed)
+print(info_attempted)
+print(info_performed)
+print(info_performed_compiled)
