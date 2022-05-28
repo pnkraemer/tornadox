7b5d5e25f7217d8035eac14324ab3199ef35552e
Nicholas Krämer <nicokraemer@gmx.net>
2022-05-28 12:37:28 +0200

    EK1 step

---
+++ tests/test_building_blocks/test_step_implementations/test_ek1_projmatfree_d_nu.py
@@ -0,0 +1,103 @@
+"""Tests for traditional EK1 implementations."""
+
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+import pytest
+
+from tornadox.blocks.sde import ibm
+from tornadox.blocks.step_impl.first_order import ek1_projmatfree_d_nu
+
+
+@partial(jax.jit, static_argnames=("n", "d"))
+def _setup(*, n, d):
+    """Setup for derivative-dimension ordering."""
+
+    # Preconditioners
+    p, p_inv = ibm.preconditioner_diagonal(dt=0.1, num_derivatives=n)
+    p, p_inv = jnp.tile(p, reps=d), jnp.tile(p_inv, reps=d)
+
+    # System matrices
+    eye_d = jnp.eye(d)
+    a, q_sqrtm = ibm.system_matrices_1d(num_derivatives=n)
+    a, q_sqrtm = jnp.kron(eye_d, a), jnp.kron(eye_d, a)
+
+    # Projection matrices
+    e0, e1, *_ = jnp.eye(n + 1)[:, None, :]
+    e0, e1 = jnp.kron(eye_d, e0), jnp.kron(eye_d, e1)
+
+    # Some mean and covariance
+    m = jnp.arange(1, 1 + d * (n + 1))
+    c_sqrtm = jnp.arange(1, 1 + d**2 * (n + 1) ** 2).reshape(
+        (d * (n + 1), d * (n + 1))
+    )
+
+    return (p, p_inv), (a, q_sqrtm), (e0, e1), (m, c_sqrtm)
+
+
+@pytest.mark.parametrize("n", (3,))
+@pytest.mark.parametrize("d", (2,))
+def test_ek1_attempt_step_projmatfree_d_nu_forward_only(n, d):
+    (p, p_inv), (a, q_sqrtm), _, (m, c_sqrtm) = _setup(n=n, d=d)
+    out = ek1_projmatfree_d_nu.attempt_step_forward_only(
+        f=lambda x: jnp.flip(x) * (1 - x),
+        df=lambda x: 1 - 2 * x,
+        m=m,
+        c_sqrtm=c_sqrtm,
+        p=p,
+        p_inv=p_inv,
+        a=a,
+        q_sqrtm=q_sqrtm,
+        num_derivatives=n,
+    )
+
+    cor, obs, err = out
+
+    m, c_sqrtm = cor
+    assert m.shape == (d * (n + 1),)
+    assert c_sqrtm.shape == (d * (n + 1), d * (n + 1))
+
+    m, c_sqrtm = obs
+    assert m.shape == (d,)
+    assert c_sqrtm.shape == (d, d)
+
+    assert err.shape == (d,)
+
+
+@pytest.mark.parametrize("n", (3,))
+@pytest.mark.parametrize("d", (2,))
+def test_ek1_attempt_step_projmatfree_d_nu(n, d):
+    (p, p_inv), (a, q_sqrtm), (e0, e1), (m, c_sqrtm) = _setup(n=n, d=d)
+    out = ek1_projmatfree_d_nu.attempt_step(
+        f=lambda x: jnp.flip(x) * (1 - x),
+        df=lambda x: 1 - 2 * x,
+        m=m,
+        c_sqrtm=c_sqrtm,
+        p=p,
+        p_inv=p_inv,
+        a=a,
+        q_sqrtm=q_sqrtm,
+        num_derivatives=n,
+    )
+
+    cor, obs, ext, bw, err = out
+
+    m, c_sqrtm = cor
+    assert m.shape == (d * (n + 1),)
+    assert c_sqrtm.shape == (d * (n + 1), d * (n + 1))
+
+    m, c_sqrtm = ext
+    assert m.shape == (d * (n + 1),)
+    assert c_sqrtm.shape == (d * (n + 1), d * (n + 1))
+
+    m, c_sqrtm = obs
+    assert m.shape == (d,)
+    assert c_sqrtm.shape == (d, d)
+
+    g, (m, c_sqrtm) = bw
+    assert m.shape == (d * (n + 1),)
+    assert c_sqrtm.shape == (d * (n + 1), d * (n + 1))
+    assert g.shape == (d * (n + 1), d * (n + 1))
+
+    assert err.shape == (d,)
---
+++ tornadox/blocks/step_impl/first_order/ek1_projmatfree_d_nu.py
@@ -0,0 +1,147 @@
+"""Projection-matrix-free version of the EK1."""
+
+
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+
+from tornadox.blocks import sqrtutil
+
+
+@partial(jax.jit, static_argnames=("f", "df", "num_derivatives"))
+def attempt_step_forward_only(
+    *, f, df, m, c_sqrtm, p, p_inv, a, q_sqrtm, num_derivatives
+):
+    """Attempt a full step.
+
+    Extrapolate-then-correct.
+    Includes error estimation.
+    Includes a scalar, time-varying diffusion.
+
+    Assumes a dimension-derivative ordering.
+    """
+
+    # Apply the pre-conditioner
+    m, c_sqrtm = p_inv * m, p_inv[:, None] * c_sqrtm
+
+    # Predict mean and compute error
+    m_ext = p * (a @ m)
+
+    # Linearise ODE and estimate error
+    fx, Jx, m_obs = _linearise_ode_projmatfree(
+        f=f, df=df, m_ext=m_ext, num_derivatives=num_derivatives
+    )
+
+    err, diff_sqrtm = _estimate_error_projmatfree(
+        Jx=Jx,
+        m_res=m_obs,
+        q_sqrtm=p[:, None] * q_sqrtm,
+        num_derivatives=num_derivatives,
+    )
+
+    # The full extrapolation:
+    c_sqrtm_ext = sqrtutil.sum_of_sqrtm_factors(S1=a @ c_sqrtm, S2=diff_sqrtm * q_sqrtm)
+
+    # Un-apply the pre-conditioner
+    c_sqrtm_ext = p[:, None] * c_sqrtm_ext
+
+    # The final correction:
+    h_mul_c_sqrtm = (
+        c_sqrtm_ext[1 :: (num_derivatives + 1), :]
+        - Jx @ c_sqrtm_ext[0 :: (num_derivatives + 1)]
+    )
+    c_sqrtm_obs, (c_sqrtm_cor, g) = sqrtutil.correct_noisefree_matfree(
+        c_sqrtm=c_sqrtm_ext, h_mul_c_sqrtm=h_mul_c_sqrtm
+    )
+    m_cor = m_ext - g @ m_obs
+
+    return (m_cor, c_sqrtm_cor), (m_obs, c_sqrtm_obs), err
+
+
+@partial(jax.jit, static_argnames=("f", "df", "num_derivatives"))
+def attempt_step(*, f, df, m, c_sqrtm, p, p_inv, a, q_sqrtm, num_derivatives):
+    """Attempt a full step.
+
+    Extrapolate-then-correct.
+    Includes error estimation.
+    Includes the backward transition, but no dense output.
+    Includes a scalar, time-varying diffusion.
+    """
+
+    # Apply the pre-conditioner
+    m, c_sqrtm = p_inv * m, p_inv[:, None] * c_sqrtm
+
+    # Predict mean and compute error
+    m_ext = a @ m
+    fx, Jx, m_obs = _linearise_ode_projmatfree(
+        f=f, df=df, m_ext=p * m_ext, num_derivatives=num_derivatives
+    )
+    err, diff_sqrtm = _estimate_error_projmatfree(
+        Jx=Jx,
+        m_res=m_obs,
+        q_sqrtm=p[:, None] * q_sqrtm,
+        num_derivatives=num_derivatives,
+    )
+
+    # The full extrapolation:
+    c_sqrtm_ext, (c_sqrtm_bw, g_bw) = sqrtutil.correct_noisy(
+        c_sqrtm=c_sqrtm, h=a, r_sqrtm=diff_sqrtm * q_sqrtm
+    )
+    m_bw = m - g_bw @ m_ext
+
+    # Un-apply the pre-conditioner
+    m_ext, c_sqrtm_ext = p * m_ext, p[:, None] * c_sqrtm_ext
+    m_bw, c_sqrtm_bw = p * m_bw, p[:, None] * c_sqrtm_bw
+    g_bw = p[:, None] * g_bw * p_inv[None, :]
+    backward_model = (g_bw, (m_bw, c_sqrtm_bw))
+
+    # The final correction:
+    h_mul_c_sqrtm = (
+        c_sqrtm_ext[1 :: (num_derivatives + 1), :]
+        - Jx @ c_sqrtm_ext[0 :: (num_derivatives + 1)]
+    )
+    c_sqrtm_obs, (c_sqrtm_cor, g) = sqrtutil.correct_noisefree_matfree(
+        c_sqrtm=c_sqrtm_ext, h_mul_c_sqrtm=h_mul_c_sqrtm
+    )
+    m_cor = m_ext - g @ m_obs
+
+    return (
+        (m_cor, c_sqrtm_cor),
+        (m_obs, c_sqrtm_obs),
+        (m_ext, c_sqrtm_ext),
+        backward_model,
+        err,
+    )
+
+
+@partial(jax.jit, static_argnames=("f", "df", "num_derivatives"))
+def _linearise_ode_projmatfree(*, f, df, m_ext, num_derivatives):
+    m_at = m_ext[0 :: (num_derivatives + 1)]
+
+    fx = f(m_at)
+    Jx = df(m_at)
+    m_obs = m_ext[1 :: (num_derivatives + 1)] - fx
+
+    return fx, Jx, m_obs
+
+
+@partial(jax.jit, static_argnames=("num_derivatives",))
+def _estimate_error_projmatfree(*, Jx, m_res, q_sqrtm, num_derivatives):
+    s_sqrtm = (
+        q_sqrtm[1 :: (num_derivatives + 1), :]
+        - Jx @ q_sqrtm[0 :: (num_derivatives + 1), :]
+    )
+    s_chol = sqrtutil.sqrtm_to_cholesky(St=s_sqrtm.T)
+
+    res_white = jax.scipy.linalg.solve_triangular(s_chol.T, m_res, lower=False)
+    diff = res_white.T @ res_white / res_white.size
+    diff_sqrtm = jnp.sqrt(diff)
+
+    error_estimate = diff_sqrtm * jnp.sqrt(jnp.einsum("jk,jk->j", s_chol, s_chol))
+    return error_estimate, diff_sqrtm
+
+
+@jax.jit
+def _square(x):
+    return x @ x.T
