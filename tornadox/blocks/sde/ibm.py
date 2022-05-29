fc169dad6b4db153aec2efdba83d4f7342b48ad8
Nicholas Krämer <nicokraemer@gmx.net>
2022-05-29 08:24:04 +0200

    Clarified Hilbert() and Pascal() function names

--- tornadox/blocks/sde/ibm.py
+++ tornadox/blocks/sde/ibm.py
@@ -7,15 +7,6 @@ import jax.numpy as jnp
 
 
 @partial(jax.jit, static_argnames=("num_derivatives",))
-def system_matrices_1d(*, num_derivatives):
-    x = jnp.arange(num_derivatives + 1)
-
-    A_1d = jnp.flip(_pascal(x)[0])  # no idea why the [0] is necessary...
-    Q_1d = jnp.flip(_hilbert(x))
-    return A_1d, jnp.linalg.cholesky(Q_1d)
-
-
-@partial(jax.jit, static_argnames=("num_derivatives",))
 def preconditioner_diagonal(*, dt, num_derivatives):
     powers = jnp.arange(num_derivatives, -1, -1)
 
@@ -28,17 +19,37 @@ def preconditioner_diagonal(*, dt, num_derivatives):
     return scaling_vector, scaling_vector_inv
 
 
+@partial(jax.jit, static_argnames=("num_derivatives",))
+def system_matrices_1d(*, num_derivatives):
+    x = jnp.arange(num_derivatives + 1)
+
+    A_1d = jnp.flip(
+        _arange_to_pascal_matrix(x)[0]
+    )  # no idea why the [0] is necessary...
+    Q_1d = jnp.flip(_arange_to_hilbert_matrix(x))
+    return A_1d, jnp.linalg.cholesky(Q_1d)
+
+
 @jax.jit
-def _hilbert(a):
+def _arange_to_hilbert_matrix(a):
     return 1 / (a[:, None] + a[None, :] + 1)
 
 
 @jax.jit
-def _pascal(a):
+def _arange_to_pascal_matrix(a):
     return _binom(a[:, None], a[None, :])
 
 
 def _batch_gram(k):
+    r"""Batch a function.
+
+    A function :math:`f: R^k \times R^k \rightarrow R` becomes a function
+
+        .. math: g: R^{n \times k} \times R^{k \times m} \rightarrow R^{n \times m}.
+
+    The batching follows broadcasting rules:
+    think `jnp.exp(x[:, None], y[None, :])` for 1d inputs.
+    """
     k_vmapped_x = jax.vmap(k, in_axes=(0, None), out_axes=-1)
     k_vmapped_xy = jax.vmap(k_vmapped_x, in_axes=(None, 1), out_axes=-1)
     return jax.jit(k_vmapped_xy)
